d03809a08e06f70bcfaa13ae00d0c0c524244b03
praneshkA <pranesh.ka2023it@sece.ac.in>
2026-06-10 18:56:02 +0530

    Fix Myorder import issue

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Shop from './Pages/Shop';
 import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product';
 import Cart from './Pages/Cart';
-import MyOrder from './Pages/Myorder';
+import Myorder from './Pages/Myorder';
 function App() {
   return (
     <div>
@@ -29,7 +29,7 @@ function App() {
           {/* <Route path="/product" element={<Product />} /> */}
           <Route path='/product/:productId' element={<Product />} />
           <Route path='/cart' element={<Cart />} />
-<Route path="/login" element={<LoginSignup />} />          <Route path='/myorder' element={<MyOrder />} />
+<Route path="/login" element={<LoginSignup />} />          <Route path='/myorder' element={<Myorder />} />
 
         </Routes>
 
